216fd9dcdb88dc6ac9c45b7bfbba6585ea395e71
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-02-26 12:58:05 +0300

    onjects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: "job",
+  streetAddress: "75 luthuli",
+};
+function updateEmployeeWithKeyAndValue(object, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]; // Remove the key from the object
+  return employee;
+}
+console.log(updateEmployeeWithKeyAndValue());
